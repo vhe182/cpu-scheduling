b45d6d45d22fff5932522bc95c33211631ef2198
Victor Estrada <vhe182@gmail.com>
2017-02-02 23:22:12 -0600

    Comment the steps for the program
    Also added arg printout to confirm args passed in.
    This will be removed at a later date.

--- src/develop/Main.java
+++ src/develop/Main.java
@@ -3,8 +3,21 @@ package develop;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Hello World");
+        System.out.println("cpu-scheduling with threads and semaphores");
+        for ( int i = 0 ; i < args.length ; i++ ){
+            System.out.printf("%s ", args[i]);
+        }
 
+        // Call the file reader thread
+        // Wait for file reader thread to terminate
+
+        // Output metrics
+        return;
     }
+
+    // File Reader Thread
+
+    // CPU Scheduling Thread
+
+    // IO System Thread
 }
