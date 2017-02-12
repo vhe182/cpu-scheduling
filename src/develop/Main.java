f90c48e1c3ec5fe47dc874daa54a0e06c33c0ffb
Victor Estrada <victor.estrada@rackspace.com>
2017-02-12 11:09:52 -0600

    Add static LinkedList queues for all threads
    - Also changed Flag variable access to use the correct method in
      accordance with change to private variable.
    - Fixed Typo
    - Moved printFlag() command to inside the try/catch

--- src/develop/Main.java
+++ src/develop/Main.java
@@ -1,34 +1,45 @@
 package develop;
 
+import java.util.LinkedList;
+
 import static java.lang.System.exit;
 
 public class Main {
 
+    // Static queues that are shared between threads
+    public static LinkedList<Proc> ready_queue;
+    public static LinkedList<Proc> io_queue;
+    public static Flag gflags;
+
     public static void main(String[] args) {
-        Flag gflags = null;
+        gflags = null;
         System.out.println("cpu-scheduling with threads and semaphores");
 
         // Create a new Flag object and pass it the arguments for parsing.
         // Flag object throws various Exceptions for invalid arguments.
         try{
             gflags = new Flag( args );
+            gflags.printFlags();
         }catch( Exception e ){
             System.out.println(e);
             exit(0);
         }
-        gflags.printFlags();
+
+        // Create Ready and Waiting Queues
+        ready_queue = new LinkedList<>();
+        io_queue = new LinkedList<>();
 
         // Create Reader Thread
         ReaderThread readerthread = null;
         try{
-            readerthread = new ReaderThread( gflags.inputfile );
+            readerthread = new ReaderThread( gflags.getInputFile() );
             readerthread.start();
             readerthread.join();
         }catch ( Exception e ){
             System.out.println(e);
         }
 
-        System.out.println("MainThread joineded with ReaderThread");
+        System.out.println("MainThread joined with ReaderThread");
 
 
 
